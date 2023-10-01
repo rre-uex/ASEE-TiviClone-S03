514657361988c138cac5528ae89426f20099f640
RobertoRE <rrecheve@gmail.com>
2023-10-01 20:43:03 +0200

    exercise 03

--- app/src/main/java/es/unex/giiis/asee/tiviclone/view/home/HomeActivity.kt
+++ app/src/main/java/es/unex/giiis/asee/tiviclone/view/home/HomeActivity.kt
@@ -5,6 +5,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 
 import es.unex.giiis.asee.tiviclone.R
@@ -13,6 +16,11 @@ import es.unex.giiis.asee.tiviclone.model.User
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private val navController by lazy {
+        (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
+    }
+
 
     companion object {
         const val USER_INFO = "USER_INFO"
@@ -40,11 +48,22 @@ class HomeActivity : AppCompatActivity() {
     }
 
     fun setUpUI(user: User) {
-        with(binding) {
-            val navHostFragment =
-                supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-            binding.bottomNavigation.setupWithNavController(navHostFragment.navController)
-        }
+        binding.bottomNavigation.setupWithNavController(navController)
+            appBarConfiguration = AppBarConfiguration(
+                setOf(
+                    R.id.discoverFragment,
+                    R.id.userFragment,
+                    R.id.libraryFragment
+                )
+            )
+        setSupportActionBar(binding.toolbar)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
     }
 
     fun setUpListeners() {
